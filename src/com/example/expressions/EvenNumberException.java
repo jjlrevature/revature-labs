3ecc199b8b90e09a9ef5a78fc302f06be825f8b1
Jesse Lauesen <jesse.lauesen@revature.net>
2021-07-09 14:05:23 -0500

    update

---
+++ src/com/example/expressions/EvenNumberException.java
@@ -0,0 +1,8 @@
+package com.example.expressions;
+
+public class EvenNumberException extends Exception {
+
+	@Override
+	public String getMessage()
+	return "Custom error message here";
+}
---
+++ src/stacks/Arrays.java
@@ -0,0 +1,11 @@
+package stacks;
+
+public class Arrays {
+
+	public static String toString(int[] items) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
